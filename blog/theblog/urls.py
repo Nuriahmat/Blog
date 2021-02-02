3e527efb444f8c2a0f051c13dd5b8ff4383c23a0
nueraihemaiti-k <nueraihemaiti-k@dmm.com>
2021-02-02 12:26:33 +0900

    Add category page

--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 # from . import views
-from .views import HomeView, DetailView, AddPostView, updatePostView, deletePostView
+from .views import HomeView, DetailView, AddPostView, updatePostView, deletePostView, AddCategoryView, CategoryView
 
 urlpatterns = [
     # path('', views.home, name="home"),
@@ -10,4 +10,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('article/edit/<int:pk>', updatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', deletePostView.as_view(), name="delete_post"),
+    path('add_category/', AddCategoryView.as_view(), name="add_category"),
+    path('category/<str:category_name>', CategoryView, name="category"),
 ]
--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
 # def home(request):
 #     return render(request, 'home.html', {})
 
+
 class HomeView(ListView):
     model = Post
     template_name = "home.html"
-    ordering = ['post_date']
+    ordering = ['-post_date']
+
+
+def CategoryView(request, category_name):
+    category_post = Post.objects.filter(category=category_name)
+    return render(request, "categories.html", {'category_name': category_name, 'category_post': category_post})
+
 
 class DetailView(DetailView):
     model = Post
     template_name = "detail.html"
+
 
 class AddPostView(CreateView):
     model = Post
@@ -23,12 +31,20 @@ class AddPostView(CreateView):
     # fields = '__all__'
     # fields = ('title', 'body')
 
+
 class updatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = "update_post.html"
 
+
 class deletePostView(DeleteView):
     model = Post
     template_name = "delete_post.html"
     success_url = reverse_lazy('home')
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = "add_category.html"
+    fields = '__all__'
